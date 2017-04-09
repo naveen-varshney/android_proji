b9200b354d4c9b1ea8ffdd64c43b2ec2eb0a8bf3
Naveen Varshney <Naveen Varshney>
2017-04-09 18:44:25 +0530

    first android commit

---
+++ app/src/main/java/com/example/zero/firstapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.zero.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycle);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        String[] myDataset ={"hello","hello","hello","hello","hello"};
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
---
+++ app/src/main/java/com/example/zero/firstapp/Mydata.java
@@ -0,0 +1,20 @@
+package com.example.zero.firstapp;
+
+/**
+ * Created by ZERO on 4/9/2017.
+ */
+public class Mydata {
+    private String name;
+
+    public Mydata(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
